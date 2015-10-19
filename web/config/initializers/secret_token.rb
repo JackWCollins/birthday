d7bebc52df0267b015176a895090fcd57f46ee53
Jack Collins <BlueSkyFlyer2@gmail.com>
2015-10-18 20:01:44 -0600

    Combine rails and ionic into one project

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Birthdays::Application.config.secret_token = '99e711b2b2df6b7c241de23de7e242c283986c60553e3e9470018558d289a1c0b0d45791439ce5807dd135caa2ec8f492597e31aaa7383036eee329967c0a619'
